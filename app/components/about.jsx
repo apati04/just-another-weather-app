3c158a7051d752fa65cda375fb6bae9214aab0fa
Andrew Patipaksiri <apax714@gmail.com>
2016-10-05 01:20:56 -0700

    [style] about page

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -2,4 +2,16 @@ import React, {Component} from 'react';
 
 
 
-export default (props)=> <h3>About Components</h3>
+export default (props)=>{
+  return (
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>Here are the tools and resources used to build this app</p>
+      <ul>
+      <li><a href="http://foundation.zurb.com/">Foundation</a> - CSS Framework</li>
+        <li><a href="https://facebook.github.io/react/">React</a> - UI</li>
+        <li><a href="http://openweathermap.org/">Open Weather Map</a> - Weather API</li>
+        </ul>
+    </div>
+  )
+}
